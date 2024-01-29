ae01a4e3038bc9c1462a20b1e29be2eb0833e382
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-01-29 18:54:32 -0500

    added seeds for roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,15 @@ VALUES  (1, "Reception"),
         (3, "HR"),
         (4, "Management"),
         (5, "Accounting");
+
+-- seeds roles table --
+
+INSERT INTO roles (id, title, salary, dept_id)
+VALUES  (11, "Receptionist", 32000.00, 1),
+        (21, "Lead Salesman", 50000.00, 2),
+        (22, "Salesman", 45000.00, 2),
+        (31, "Human Resources Agent", 50000.00, 3),
+        (41, "Regional Manager", 75000.00, 4),
+        (42, "Assistant to the Regional Manager", 60000.00, 4),
+        (51, "Lead Accountant", 55000.00, 5),
+        (52, "Accountant", 50000.00, 5);
