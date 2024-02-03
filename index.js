a021ad727d5d82c84dbc23cc783f0362f1798085
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-02-03 18:49:01 -0500

    updated index.js and added switch cases with console logs for testing

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 /////////////////////  OPTIONS LIST  ///////////////////////////
-const options = [
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'options1',
-        choices: [
+const mainMenu = async () => {
+    const choices = [
         'View All Employees',
         'Add Employee',
         'Update Employee Role',
@@ -16,16 +12,54 @@ const options = [
         'View All Departments',
         'Add Department',
         'Quit',
-        ],
-    },
-]
+    ];
+    const { action } = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'Select an action:',
+        choices,
+    });
 
+/////////////  SWITCH CASES  ////////////////////
+    switch (action) {
+        case 'View All Employees':
+            console.log('YOU SELECTED: View All Employees');
+            break;
+
+        case 'Add Employee':
+            console.log('YOU SELECTED: Add Employee');
+            break;
+
+        case 'Update Employee Role':
+            console.log('YOU SELECTED: Update Employee Role');
+            break;
+
+        case 'View All Roles':
+            console.log('YOU SELECTED: View All Roles');
+            break;
+
+        case 'Add Role':
+            console.log('YOU SELECTED: Add Role');
+            break;
+
+        case 'View All Departments':
+            console.log('YOU SELECTED: View All Departments');
+            break;
+
+        case 'Add Department':
+            console.log('YOU SELECTED: Add Department');
+            break;
+
+        case 'Quit':
+            process.exit();
+    }
+//////////////////////  CALL MENU AFTER SELECTION, TEMPORARY FOR TESTING  ////////////////////////////////////////
+    mainMenu();
+};
 
 function init() {
-    console.log('Init function called!');
-//    inquirer.prompt(questions).then((answers) => {
-//      writeToFile('logo.svg', answers);
-//    });
-  }
+    console.log('Welcome to EMPLOYEE MANAGER 2K24!!!');
+    mainMenu();
+}
 
 init();
